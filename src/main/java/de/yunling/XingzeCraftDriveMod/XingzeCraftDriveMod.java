da1e05baf27266a33b3d9815b94f60f78f4c131a
云玲 <47023706+AlwaysGuu@users.noreply.github.com>
2022-04-05 17:30:54 +0800

    ModInitializer

---
+++ src/main/java/de/yunling/XingzeCraftDriveMod/XingzeCraftDriveMod.java
@@ -0,0 +1,27 @@
+package de.yunling.XingzeCraftDriveMod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class XingzeCraftDriveMod implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public static final Block Station_Sign_MC01 = new Block(FabricBlockSettings.of(Material.METAL).hardness(4.0F));
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		Registry.register(Registry.BLOCK, new Identifier("xcdm","station_sign_mc01"),Station_Sign_MC01);
+		LOGGER.info("XingzeCraftDriveMod is loaded!");
+	}
+}
