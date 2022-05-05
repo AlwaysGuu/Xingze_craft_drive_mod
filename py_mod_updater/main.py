1c507e5e272adcbc06f6c04cda3ee8cf1152c74d
云玲 <47023706+AlwaysGuu@users.noreply.github.com>
2022-05-05 18:20:28 +0800

    Updater

--- py_mod_updater/main.py
+++ py_mod_updater/main.py
@@ -1,4 +1,4 @@
-import json,urllib.request,os,sys,urllib3,hashlib
+import json,urllib.request,os,sys,hashlib
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 opener=urllib.request.build_opener()
@@ -6,18 +6,18 @@ opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKi
 urllib.request.install_opener(opener)
 
 
-print("Xingze Craft Mod Updater v0.1")
+print("星泽工艺服务器Mod更新器 v0.1")
 
-print("Self Check...")
+print("正在自检...")
 
 if not( os.path.exists('MTR.jar')) :
-    print("Can't found MTR mod. Do you want to download?")
-    print("d - Download c - Cancel")
-    if input("Choose:") == "d":
+    print("无法找到MTR mod，是否要下载？")
+    print("d - 下载 c - 取消")
+    if input("选择（输入字母后回车）:") == "d":
         try:
             urllib.request.urlretrieve("https://yunling.de/files/MTR_Server/MTR.jar","MTR.jar")
         except Exception as ex:
-            print("Error!")
+            print("下载失败！错误信息：")
             print(ex)
             input()
             sys.exit()
@@ -25,13 +25,27 @@ if not( os.path.exists('MTR.jar')) :
         sys.exit()
 
 if not( os.path.exists('joban.jar')) :
-    print("Can't found joban mod. Do you want to download?")
-    print("d - Download c - Cancel")
-    if input("Choose:") == "d":
+    print("无法找到joban mod，是否要下载？")
+    print("d - 下载 c - 取消")
+    if input("选择（输入字母后回车）:") == "d":
         try:
             urllib.request.urlretrieve("https://yunling.de/files/MTR_Server/joban.jar","joban.jar")
         except Exception as ex:
-            print("Error!")
+            print("下载失败!")
+            print(ex)
+            input()
+            sys.exit()
+    else:
+        sys.exit()
+
+if not( os.path.exists('XZD.jar')) :
+    print("无法找到驱动 mod，是否要下载？")
+    print("d - 下载 c - 取消")
+    if input("选择（输入字母后回车）:") == "d":
+        try:
+            urllib.request.urlretrieve("https://yunling.de/files/MTR_Server/XZD.jar","XZD.jar")
+        except Exception as ex:
+            print("下载失败!")
             print(ex)
             input()
             sys.exit()
@@ -40,13 +54,13 @@ if not( os.path.exists('joban.jar')) :
 
 
 
-print("Getting Updates...")
+print("正在检查更新...")
 
 try:
     urllib.request.urlretrieve("https://yunling.de/files/MTR_Server/md5.txt","md5.txt")
 
 except Exception as ex:
-    print("Error to getting upadtes!")
+    print("mod md5值拉取失败！")
     print(ex)
     input()
     sys.exit()
@@ -54,42 +68,64 @@ except Exception as ex:
 with open('md5.txt',"r") as f:
     MTR_MD5 = f.readlines()[0].strip()
 
-    print("Latest MTR mod MD5:"+MTR_MD5)
+    #print("Latest MTR mod MD5:"+MTR_MD5)
 
 
 with open('md5.txt',"r") as f:
 
     j_MD5 = f.readlines()[1].strip()
 
-    print("Latest joban mod MD5:"+j_MD5)
+    #print("Latest joban mod MD5:"+j_MD5)
+
+with open('md5.txt',"r") as f:
+
+    D_MD5 = f.readlines()[2].strip()
+
+    #print("Latest Drive mod MD5:"+D_MD5)
 
 
 with open('MTR.jar',"rb") as f:
     file = f.read()
 if hashlib.md5(file).hexdigest() != MTR_MD5:
-    print("Server has new MTR mod, downloading...")
+    print("有新的MTR mod，正在下载更新...")
     try:
             urllib.request.urlretrieve("https://yunling.de/files/MTR_Server/MTR.jar","MTR.jar")
     except Exception as ex:
-        print("Error!")
+        print("下载失败!")
         print(ex)
         input()
         sys.exit()
 else:
-    print("Your MTR mod is latest")
+    print("MTR mod为最新")
 
 with open('joban.jar',"rb") as f:
     file = f.read()
 if hashlib.md5(file).hexdigest() != j_MD5:
-    print("Server has new joban mod, downloading...")
+    print("有新的Joban mod，正在下载更新...")
     try:
             urllib.request.urlretrieve("https://yunling.de/files/MTR_Server/joban.jar","joban.jar")
     except Exception as ex:
-        print("Error!")
+        print("下载失败!")
         print(ex)
         input()
         sys.exit()
 else:
-    print("Your Joban mod is latest")
+    print("Joban mod为最新")
 
-print("All mods are latest!, Have a fun!")
+with open('XZD.jar',"rb") as f:
+    file = f.read()
+if hashlib.md5(file).hexdigest() != D_MD5:
+    print("有新的驱动 mod，正在下载更新...")
+    try:
+            urllib.request.urlretrieve("https://yunling.de/files/MTR_Server/XZD.jar","XZD.jar")
+    except Exception as ex:
+        print("下载失败!")
+        print(ex)
+        input()
+        sys.exit()
+else:
+    print("驱动 mod为最新")
+
+
+print("全部mod为最新")
+input("按任意键退出...")
